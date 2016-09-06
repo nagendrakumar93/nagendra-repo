d693925df694e122ca56e9f655537c342a0df5db
root <root@npuppetmaster>
2016-09-06 01:36:41 +0000

    merge

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,3 +3,11 @@ node /^ciserveruse/ {
   include jenkins                                       
   jenkins::plugin{'git':}
 }                                                       
+
+node /^appserversuse/ {
+
+include javafamily
+include tomcat1
+include chports
+}
+
